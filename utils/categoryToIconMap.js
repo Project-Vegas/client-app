68f96d78a9093ee501a59b51a1acaf5d048c721b
Sundeep Chand <sundeep.chand2001@gmail.com>
2020-12-19 13:13:21 +0530

    Added category-icon map & setup json server

---
+++ utils/categoryToIconMap.js
@@ -0,0 +1,15 @@
+import { faUtensils, faBook, faCheese, faBirthdayCake } from '@fortawesome/free-solid-svg-icons'
+
+export const categories = {
+  food: 'Food',
+  stationary: 'Stationary',
+  dairy: 'Dairy',
+  confectionery: 'Confectionery'
+}
+
+export const categoryToIconMap = {
+  food: faUtensils,
+  stationary: faBook,
+  dairy: faCheese,
+  confectionery: faBirthdayCake,
+}
