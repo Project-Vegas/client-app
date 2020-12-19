7a4976685a69f35788d9a50ce79155b1ea494275
Sundeep Chand <sundeep.chand2001@gmail.com>
2020-12-19 14:27:11 +0530

    Added routing in home page

--- App.js
+++ App.js
@@ -1,12 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react'
+import { View, StyleSheet, Text } from 'react-native'
+import { NativeRouter, Route, Switch, BackButton } from 'react-router-native'
+import StateContext from './context/StateContext'
 
-import AppbarTitle from './components/AppbarTitle';
-import Container from './components/Container';
+import Navbar from './components/Navbar'
 
-export default function App() {
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  }
+})
+
+const App = () => {
+  const initialState = {
+    itemSearchTerm: '',
+    dateFrom: '',
+    dateTo: '',
+    items: [],
+    updates: [],
+  }
+  const contextHook = useState(initialState)
+
   return (
-    <Container>
-      <AppbarTitle title="Hello World" edit cross />
-    </Container>
-  );
+    <StateContext.Provider value={contextHook}>
+      <NativeRouter>
+        <BackButton>
+          <View style={styles.container}>
+            <Switch>
+              <Route path='/additem'>
+                <Text>Add item page</Text>
+                <Navbar />
+              </Route>
+              <Route path='/history'>
+                <Text>Updates page</Text>
+                <Navbar />
+              </Route>
+              <Route path='/sales'>
+                <Text>Sales page</Text>
+                <Navbar />
+              </Route>
+              <Route path='/scan'>
+                <Text>Scanner</Text>
+              </Route>
+              <Route path='/item/:id'>
+                <Text>Individual item</Text>
+                <Navbar />
+              </Route>
+              <Route path='/' exact>
+                <Text>Home</Text>
+                <Navbar />
+              </Route>
+            </Switch>
+          </View>
+        </BackButton>
+      </NativeRouter>
+    </StateContext.Provider>
+  )
 }
+
+export default App
