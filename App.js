34568fda5ab989281ee2c38bfd56d6e147fedb91
Sundeep Chand <sundeep.chand2001@gmail.com>
2020-12-19 12:58:58 +0530

    Setup the packages

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello world</Text>
+      <FontAwesomeIcon size={26} icon={faSearch} />
       <StatusBar style="auto" />
     </View>
   );
